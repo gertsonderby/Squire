726fadb06e3ca7ee5e570370e6a1dc4a2e3a59a6
Gert K. Sønderby <gert@one.com>
2015-10-01 14:30:46 +0200

    Fix for pasting into PREs. More reliable in detecting where it is.

--- build/squire-raw.js
+++ build/squire-raw.js
@@ -3469,7 +3469,7 @@ var decreaseListLevel = function ( frag ) {
     return frag;
 };
 
-var makePreformatted = function ( frag ) {
+var getTextFromHTMLFragment = function ( self, frag ) {
     var walker = getBlockWalker( frag ),
         lines = [],
         node;
@@ -3477,10 +3477,13 @@ var makePreformatted = function ( frag ) {
         // Strip down to text only
         lines.push( node.textContent );
     }
-    node = this._doc.createTextNode( lines.join( '\n' ) || '\n' );
+    return self._doc.createTextNode( lines.join( '\n' ) || '\n' );
+};
+
+var makePreformatted = function ( frag ) {
     return this.createElement( 'PRE',
         this._config.tagAttributes.pre, [
-            node
+            getTextFromHTMLFragment( this, frag )
         ] );
 };
 
@@ -3753,12 +3756,13 @@ proto.insertHTML = function ( html, isPaste ) {
         frag = this._doc.createDocumentFragment(),
         div = this.createElement( 'DIV' );
 
-    if ( this.hasFormat( 'pre' ) || this.hasFormat( 'code' ) ) {
+    // Parse HTML into DOM tree
+    div.innerHTML = html;
+    // If range is entirely inside PRE tag
+    if ( getNearest( range.commonAncestorContainer, 'PRE' ) ) {
         // Insert unparsed in text node
-        frag.appendChild( this._doc.createTextNode( html ) );
+        frag.appendChild( getTextFromHTMLFragment( this, div ) );
     } else {
-        // Parse HTML into DOM tree
-        div.innerHTML = html;
         frag.appendChild( empty( div ) );
     }
 
